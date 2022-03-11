1cb9850bd2133f6e6109fd77f1f802555041535d
Cui Ziqiang <keeper013@gmail.com>
2022-03-12 05:30:48 +0800

    Update NegativeTests.cs
    added mapping context related negative test cases

--- EntityFrameworkCore.Mapper/Oasis.EntityFrameworkCore.Mapper.Test/NegativeTests.cs
+++ EntityFrameworkCore.Mapper/Oasis.EntityFrameworkCore.Mapper.Test/NegativeTests.cs
@@ -20,6 +20,47 @@ public sealed class NegativeTests : IDisposable
     }
 
     [Fact]
+    public void MapWithoutContext_ShouldFail()
+    {
+        var factory = new MapperFactory();
+        var mapperBuilder = factory.Make(GetType().Name);
+
+        // TODO: shouldn't need to call this when cascade register is implemented
+        mapperBuilder.Register<ScalarClass2, ScalarClass1>();
+        mapperBuilder.Register<ListIClass1, CollectionClass1>();
+
+        var mapper = mapperBuilder.Build();
+
+        var cc1 = new CollectionClass1(1, 1, new List<ScalarClass1> { new ScalarClass1(1, 1, 2, "3", new byte[] { 1 }) });
+        var lic1 = new ListIClass1(1, 2, new List<ScalarClass2> { new ScalarClass2(2, 2, 3, "4", new byte[] { 2 }) });
+
+        Assert.Throws<MappingContextNotStartedException>(() => mapper.Map(lic1, cc1));
+    }
+
+    [Fact]
+    public void StartingContextWithoutDisposingExisting_ShouldFail()
+    {
+        var factory = new MapperFactory();
+        var mapperBuilder = factory.Make(GetType().Name);
+
+        // TODO: shouldn't need to call this when cascade register is implemented
+        mapperBuilder.Register<ScalarClass2, ScalarClass1>();
+        mapperBuilder.Register<ListIClass1, CollectionClass1>();
+
+        var mapper = mapperBuilder.Build();
+
+        var cc1 = new CollectionClass1(1, 1, new List<ScalarClass1> { new ScalarClass1(1, 1, 2, "3", new byte[] { 1 }) });
+        var lic1 = new ListIClass1(1, 2, new List<ScalarClass2> { new ScalarClass2(2, 2, 3, "4", new byte[] { 2 }) });
+
+        Assert.Throws<MappingContextStartedException>(() =>
+        {
+            using var context1 = mapper.StartMappingContext(_dbContext);
+            using var context2 = mapper.StartMappingContext(_dbContext);
+            mapper.Map(lic1, cc1);
+        });
+    }
+
+    [Fact]
     public void MapListProperties_UpdateNonExistingNavitation_ShouldFail()
     {
         var factory = new MapperFactory();
