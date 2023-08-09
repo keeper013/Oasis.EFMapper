1d5f3266bae49c0eaeefedf61406295f065c9875
Cui Ziqiang <keeper013@gmail.com>
2023-08-09 21:59:34 +0800

    added performance test cases

--- EntityFrameworkCore/PerformanceComparer/Program.cs
+++ EntityFrameworkCore/PerformanceComparer/Program.cs
@@ -2,8 +2,11 @@
 using PerformanceComparer.Scalar;
 
 var dict = new Dictionary<string, TimeSpan>();
-ScalarTests.EfMapper_Scalar(dict);
-ScalarTests.EfMapper_Scalar_Session(dict);
-ScalarTests.AutoMapper_Scalar(dict);
+ScalarTests.EfMapper_IdenticalItem_Scalar(dict);
+ScalarTests.EfMapper_IdenticalItem_Scalar_Session(dict);
+ScalarTests.AutoMapper_IdenticalItem_Scalar(dict);
+ScalarTests.EfMapper_SeparateItem_Scalar(dict);
+ScalarTests.EfMapper_SeparateItem_Scalar_Session(dict);
+ScalarTests.AutoMapper_SeparateItem_Scalar(dict);
 
 Utilities.Print(dict, ScalarTests.Rounds);
--- EntityFrameworkCore/PerformanceComparer/Scalar/ScalarTests.cs
+++ EntityFrameworkCore/PerformanceComparer/Scalar/ScalarTests.cs
@@ -7,37 +7,86 @@ internal static class ScalarTests
 {
     internal const int Rounds = 10000000;
 
-    public static void EfMapper_Scalar(IDictionary<string, TimeSpan> dict)
+    public static void EfMapper_IdenticalItem_Scalar(IDictionary<string, TimeSpan> dict)
     {
         var mapper = new MapperBuilderFactory().MakeMapperBuilder().Register<ScalarSource, ScalarTarget>().Build();
         var source = ScalarUtilities.BuildDefaultScalarSource();
-        using var timer = new StopWatchTimer(dict, nameof(EfMapper_Scalar));
+        using var timer = new StopWatchTimer(dict, nameof(EfMapper_IdenticalItem_Scalar));
         for (var i = 0; i < Rounds; i++)
         {
             _ = mapper.Map<ScalarSource, ScalarTarget>(source);
         }
     }
 
-    public static void EfMapper_Scalar_Session(IDictionary<string, TimeSpan> dict)
+    public static void EfMapper_SeparateItem_Scalar(IDictionary<string, TimeSpan> dict)
+    {
+        var mapper = new MapperBuilderFactory().MakeMapperBuilder().Register<ScalarSource, ScalarTarget>().Build();
+        var list = new List<ScalarSource>();
+        for (var i = 0; i < Rounds; i++)
+        {
+            list.Add(ScalarUtilities.BuildDefaultScalarSource());
+        }
+
+        using var timer = new StopWatchTimer(dict, nameof(EfMapper_SeparateItem_Scalar));
+        foreach (var item in list)
+        {
+            _ = mapper.Map<ScalarSource, ScalarTarget>(item);
+        }
+    }
+
+    public static void EfMapper_IdenticalItem_Scalar_Session(IDictionary<string, TimeSpan> dict)
     {
         var session = new MapperBuilderFactory().MakeMapperBuilder().Register<ScalarSource, ScalarTarget>().Build().CreateMappingSession();
         var source = ScalarUtilities.BuildDefaultScalarSource();
-        using var timer = new StopWatchTimer(dict, nameof(EfMapper_Scalar_Session));
+        using var timer = new StopWatchTimer(dict, nameof(EfMapper_IdenticalItem_Scalar_Session));
         for (var i = 0; i < Rounds; i++)
         {
             _ = session.Map<ScalarSource, ScalarTarget>(source);
         }
     }
 
-    public static void AutoMapper_Scalar(IDictionary<string, TimeSpan> dict)
+    public static void EfMapper_SeparateItem_Scalar_Session(IDictionary<string, TimeSpan> dict)
+    {
+        var session = new MapperBuilderFactory().MakeMapperBuilder().Register<ScalarSource, ScalarTarget>().Build().CreateMappingSession();
+        var list = new List<ScalarSource>();
+        for (var i = 0; i < Rounds; i++)
+        {
+            list.Add(ScalarUtilities.BuildDefaultScalarSource());
+        }
+
+        using var timer = new StopWatchTimer(dict, nameof(EfMapper_SeparateItem_Scalar_Session));
+        foreach (var item in list)
+        {
+            _ = session.Map<ScalarSource, ScalarTarget>(item);
+        }
+    }
+
+    public static void AutoMapper_IdenticalItem_Scalar(IDictionary<string, TimeSpan> dict)
     {
         var config = new MapperConfiguration(cfg => cfg.CreateMap<ScalarSource, ScalarTarget>());
         var mapper = new Mapper(config);
         var source = ScalarUtilities.BuildDefaultScalarSource();
-        using var timer = new StopWatchTimer(dict, nameof(AutoMapper_Scalar));
+        using var timer = new StopWatchTimer(dict, nameof(AutoMapper_IdenticalItem_Scalar));
         for (var i = 1; i < Rounds; i++)
         {
             _ = mapper.Map<ScalarSource, ScalarTarget>(source);
+        }
+    }
+
+    public static void AutoMapper_SeparateItem_Scalar(IDictionary<string, TimeSpan> dict)
+    {
+        var config = new MapperConfiguration(cfg => cfg.CreateMap<ScalarSource, ScalarTarget>());
+        var mapper = new Mapper(config);
+        var list = new List<ScalarSource>();
+        for (var i = 0; i < Rounds; i++)
+        {
+            list.Add(ScalarUtilities.BuildDefaultScalarSource());
+        }
+
+        using var timer = new StopWatchTimer(dict, nameof(AutoMapper_SeparateItem_Scalar));
+        foreach (var item in list)
+        {
+            _ = mapper.Map<ScalarSource, ScalarTarget>(item);
         }
     }
 }
