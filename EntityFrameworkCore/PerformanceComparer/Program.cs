3e0104895b078a5a7e81ea8efd27629c0dddf857
Cui Ziqiang <keeper013@gmail.com>
2023-09-27 22:45:06 +0800

    reverted file that was committed by mistake

--- EntityFrameworkCore/PerformanceComparer/Program.cs
+++ EntityFrameworkCore/PerformanceComparer/Program.cs
@@ -3,10 +3,10 @@ using PerformanceComparer.Scalar;
 
 var dict = new Dictionary<string, TimeSpan>();
 ScalarTests.EfMapper_IdenticalItem_Scalar(dict);
-//ScalarTests.EfMapper_IdenticalItem_Scalar_Session(dict);
+ScalarTests.EfMapper_IdenticalItem_Scalar_Session(dict);
 ScalarTests.AutoMapper_IdenticalItem_Scalar(dict);
-//ScalarTests.EfMapper_SeparateItem_Scalar(dict);
-//ScalarTests.EfMapper_SeparateItem_Scalar_Session(dict);
-//ScalarTests.AutoMapper_SeparateItem_Scalar(dict);
+ScalarTests.EfMapper_SeparateItem_Scalar(dict);
+ScalarTests.EfMapper_SeparateItem_Scalar_Session(dict);
+ScalarTests.AutoMapper_SeparateItem_Scalar(dict);
 
 Utilities.Print(dict, ScalarTests.Rounds);
